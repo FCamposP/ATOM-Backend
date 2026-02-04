56d4488468ced5c288c1f447ee2f047014f95a60
FCamposP <fabrycio7campos7@gmail.com>
2026-02-04 17:29:59 -0600

    se agregó description a schema para create task

--- functions/src/api/schemas/task.schema.ts
+++ functions/src/api/schemas/task.schema.ts
@@ -1,6 +1,7 @@
 import {z} from "zod";
 export const createTaskSchema = z.object({
   title: z.string().min(3, "Title must be at least 3 characters"),
+  description:z.string().optional()
 });
 
 export const updateTaskSchema= z.object({
