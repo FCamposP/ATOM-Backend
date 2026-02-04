980b1ce420d063ef0fad434567ec0ee61d0ec50d
FCamposP <fabrycio7campos7@gmail.com>
2026-02-04 08:14:20 -0600

    reubicación de src

--- src/domain/repositories/UserRepository.ts
+++
@@ -1,6 +0,0 @@
-import { IUser } from "../entities/User";
-
-export interface UserRepository {
-  create(user: IUser): Promise<IUser>;
-  findByEmail(email: string): Promise<IUser | null>;
-}
--- src/infrastructure/database/firestore.ts
+++
@@ -1,8 +0,0 @@
-import admin from "firebase-admin";
-const serviceAccount = require("./serviceAccountKey.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as any),
-});
-
-export const db = admin.firestore();
